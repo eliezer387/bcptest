48b8b699ba9d6b64090377922f16296a6ef53459
Eliezer Peña <pe.eliezer@gmail.com>
2020-08-19 11:41:06 -0500

    feat: adding loading agency-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faSquare, faCheckSquare, faCamera, faAd, faAddressBook, faCar } from '@fortawesome/free-solid-svg-icons';
+import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { faStackOverflow, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { AgencyListComponent } from './agency-list/agency-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoadingComponent } from './loading/loading.component';
+import { AgencyDetailsComponent } from './agency-details/agency-details.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgencyListComponent,
+    DashboardComponent,
+    LoadingComponent,
+    AgencyDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary){
+    library.addIcons(
+      faSquare,
+      faCheckSquare,
+      farSquare,
+      farCheckSquare,
+      faStackOverflow,
+      faGithub,
+      faMedium,
+      faCamera,
+      faAd,
+      faAddressBook,
+      faCar);
+  }
+}
