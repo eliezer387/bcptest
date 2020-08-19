cb51fe74b0797b88f30123d54e0d22d57af0a3a4
Eliezer Peña <pe.eliezer@gmail.com>
2020-08-19 11:43:21 -0500

    feat: app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgencyListComponent } from './agency-list/agency-list.component';
+import { LoadingComponent } from './loading/loading.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: '',
+    component: AgencyListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
